b818ee597d9cab655ea97353833ec81619267d35
Javier-Acosta <jacosta@catamarca.gov.ar>
2024-10-15 21:04:28 -0300

    Módulo 2: OS

--- index.mjs
+++ index.mjs
@@ -4,5 +4,7 @@ console.log('Arquitectura:', os.arch());
 console.log('Plataforma:', os.platform());
 
 console.log('Memoria total:', os.totalmem());
+
 console.log('Memoria libre:', os.freemem());
+
 console.log('Informacion de la CPU:', os.cpus());
